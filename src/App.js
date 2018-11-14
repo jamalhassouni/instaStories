f862de5f1952147aff86e4a72dae4cfd89a7027f
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-11-14 18:12:11 +0100

    Using Stories Component to show stories in app

--- src/App.js
+++ src/App.js
@@ -6,55 +6,49 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n'
-});
+import React, { Component } from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { Stories } from "./components";
 
 const stories = [
   {
-    id: '2',
-    source: require('./assets/stories/2.jpg'),
-    user: 'derek.russel',
-    avatar: require('./assets/avatars/derek.russel.png'),
+    id: "2",
+    source: require("./assets/stories/2.jpg"),
+    user: "derek.russel",
+    avatar: require("./assets/avatars/derek.russel.png")
   },
   {
-    id: '4',
-    source: require('./assets/stories/4.jpg'),
-    user: 'jmitch',
-    avatar: require('./assets/avatars/jmitch.png'),
+    id: "4",
+    source: require("./assets/stories/4.jpg"),
+    user: "jmitch",
+    avatar: require("./assets/avatars/jmitch.png")
   },
   {
-    id: '5',
-    source: require('./assets/stories/5.jpg'),
-    user: 'monicaa',
-    avatar: require('./assets/avatars/monicaa.png'),
+    id: "5",
+    source: require("./assets/stories/5.jpg"),
+    user: "monicaa",
+    avatar: require("./assets/avatars/monicaa.png")
   },
   {
-    id: '3',
-    source: require('./assets/stories/3.jpg'),
-    user: 'alexandergarcia',
-    avatar: require('./assets/avatars/alexandergarcia.png'),
+    id: "3",
+    source: require("./assets/stories/3.jpg"),
+    user: "alexandergarcia",
+    avatar: require("./assets/avatars/alexandergarcia.png")
   },
   {
-    id: '1',
-    source: require('./assets/stories/1.jpg'),
-    user: 'andrea.schmidt',
-    avatar: require('./assets/avatars/andrea.schmidt.png'),
-  },
+    id: "1",
+    source: require("./assets/stories/1.jpg"),
+    user: "andrea.schmidt",
+    avatar: require("./assets/avatars/andrea.schmidt.png")
+  }
 ];
-
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <Stories {...{ stories }} />
       </View>
     );
   }
@@ -63,18 +57,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#222222"
+  }
 });
